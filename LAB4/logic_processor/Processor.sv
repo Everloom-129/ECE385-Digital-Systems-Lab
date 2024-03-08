9142308c6af4f0df062ce66e6d76905fa154e795
Everloom-129 <Everloom-129>
2024-03-08 15:55:18 +0800

    finised logic, to debug

--- LAB4/logic_processor/Processor.sv
+++ LAB4/logic_processor/Processor.sv
@@ -16,12 +16,15 @@ module Processor (input logic   Clk,     // Internal
                                 LoadA,   // Push button 1
                                 LoadB,   // Push button 2
                                 Execute, // Push button 3
-                  input  logic [3:0]  Din,     // input data
+                  // 8 bit
+                  input  logic [7:0]  Din,     // input data
                   input  logic [2:0]  F,       // Function select
                   input  logic [1:0]  R,       // Routing select
-                  output logic [3:0]  LED,     // DEBUG
-                  output logic [3:0]  Aval,    // DEBUG
+                  // 8 bit
+                  output logic [7:0]  LED,     // DEBUG
+                  output logic [7:0]  Aval,    // DEBUG
                                 Bval,    // DEBUG
+                  // I don't quite sure about this place
                   output logic [6:0]  AhexL,
                                 AhexU,
                                 BhexL,
@@ -33,16 +36,16 @@ module Processor (input logic   Clk,     // Internal
 	 logic [1:0] R_S;
 	 logic Ld_A, Ld_B, newA, newB, opA, opB, bitA, bitB, Shift_En,
 	       F_A_B;
-	 logic [3:0] A, B, Din_S;
+	 logic [7:0] A, B, Din_S; // 8 bit
 	 
 	 
 	 //We can use the "assign" statement to do simple combinational logic
 	 assign Aval = A;
 	 assign Bval = B;
-	 assign LED = {Execute_SH,LoadA_SH,LoadB_SH,Reset_SH}; //Concatenate is a common operation in HDL
+	 assign LED = {4'b0,Execute_SH,LoadA_SH,LoadB_SH,Reset_SH}; //Concatenate is a common operation in HDL
 	 
 	 //Instantiation of modules here
-	 register_unit    reg_unit (
+	 register_unit_8    reg_unit (
                         .Clk(Clk),
                         .Reset(Reset_SH),
                         .Ld_A, //note these are inferred assignments, because of the existence a logic variable of the same name
@@ -79,27 +82,32 @@ module Processor (input logic   Clk,     // Internal
                         .Ld_A,
                         .Ld_B );
 	 HexDriver        HexAL (
-                        .In0(A[3:0]),
+                        .In0(A[7:0]),
                         .Out0(AhexL) );
 	 HexDriver        HexBL (
-                        .In0(B[3:0]),
+                        .In0(B[7:0]),
                         .Out0(BhexL) );
 								
 	 //When you extend to 8-bits, 
        //you will need more HEX drivers to view upper nibble of registers, for now set to 0
-	 HexDriver        HexAU (
-                        .In0(4'h0),
-                        .Out0(AhexU) );	
-	 HexDriver        HexBU (
-                       .In0(4'h0),
-                        .Out0(BhexU) );
+	//  HexDriver        HexAU (
+      //                   .In0(4'h0),
+      //                   .Out0(AhexU) );	
+	//  HexDriver        HexBU (
+      //                  .In0(4'h0),
+      //                   .Out0(BhexU) );
+      HexDriver HexAL (.In0(A[3:0]), .Out0(AhexL));  // Lower nibble of A
+      HexDriver HexAU (.In0(A[7:4]), .Out0(AhexU));  // Upper nibble of A
+      HexDriver HexBL (.In0(B[3:0]), .Out0(BhexL));  // Lower nibble of B
+      HexDriver HexBU (.In0(B[7:4]), .Out0(BhexU));  // Upper nibble of B
+
 								
 	  //Input synchronizers required for asynchronous inputs (in this case, from the switches)
 	  //These are array module instantiations
 	  //Note: S stands for SYNCHRONIZED, H stands for active HIGH
 	  //Note: We can invert the levels inside the port assignments
 	  sync button_sync[3:0] (Clk, {~Reset, ~LoadA, ~LoadB, ~Execute}, {Reset_SH, LoadA_SH, LoadB_SH, Execute_SH});
-	  sync Din_sync[3:0] (Clk, Din, Din_S);
+	  sync Din_sync[7:0] (Clk, Din, Din_S); // 8 bit
 	  sync F_sync[2:0] (Clk, F, F_S);
 	  sync R_sync[1:0] (Clk, R, R_S);
 	  
