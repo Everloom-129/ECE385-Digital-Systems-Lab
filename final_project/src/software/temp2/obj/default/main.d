864105dcbdd3fb6fc36d280c4e809eed1a2bea0f
everloom <Everloom-129>
2024-05-10 03:00:18 -0500

    init shit

---
+++ final_project/src/software/temp2/obj/default/main.d
@@ -0,0 +1,21 @@
+obj/default/main.o: main.c \
+ d\:\intelfpga_lite\gyh-work\labs_and_reports\lab8\lab8new\lab8\software\temp2_bsp\hal\inc\io.h \
+ d\:\intelfpga_lite\gyh-work\labs_and_reports\lab8\lab8new\lab8\software\temp2_bsp\hal\inc\alt_types.h \
+ d\:/intelFPGA_lite/GYH-WORK/Labs_and_reports/LAB8/lab8new/LAB8/software/temp2_bsp/system.h \
+ d\:/intelFPGA_lite/GYH-WORK/Labs_and_reports/LAB8/lab8new/LAB8/software/temp2_bsp/linker.h \
+ d\:\intelfpga_lite\gyh-work\labs_and_reports\lab8\lab8new\lab8\software\temp2_bsp\hal\inc\alt_types.h \
+ d\:\intelfpga_lite\gyh-work\labs_and_reports\lab8\lab8new\lab8\software\temp2_bsp\hal\inc\sys\alt_irq.h \
+ d\:\intelfpga_lite\gyh-work\labs_and_reports\lab8\lab8new\lab8\software\temp2_bsp\hal\inc\nios2.h \
+ io_handler.h cy7c67200.h usb.h lcp_cmd.h lcp_data.h
+d\:\intelfpga_lite\gyh-work\labs_and_reports\lab8\lab8new\lab8\software\temp2_bsp\hal\inc\io.h:
+d\:\intelfpga_lite\gyh-work\labs_and_reports\lab8\lab8new\lab8\software\temp2_bsp\hal\inc\alt_types.h:
+d\:/intelFPGA_lite/GYH-WORK/Labs_and_reports/LAB8/lab8new/LAB8/software/temp2_bsp/system.h:
+d\:/intelFPGA_lite/GYH-WORK/Labs_and_reports/LAB8/lab8new/LAB8/software/temp2_bsp/linker.h:
+d\:\intelfpga_lite\gyh-work\labs_and_reports\lab8\lab8new\lab8\software\temp2_bsp\hal\inc\alt_types.h:
+d\:\intelfpga_lite\gyh-work\labs_and_reports\lab8\lab8new\lab8\software\temp2_bsp\hal\inc\sys\alt_irq.h:
+d\:\intelfpga_lite\gyh-work\labs_and_reports\lab8\lab8new\lab8\software\temp2_bsp\hal\inc\nios2.h:
+io_handler.h:
+cy7c67200.h:
+usb.h:
+lcp_cmd.h:
+lcp_data.h:
